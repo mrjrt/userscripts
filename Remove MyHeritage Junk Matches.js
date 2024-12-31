c3cac7763d763eaf8cd1d729b02b6a46195ae471
mrjrt <github@jamie-thompson.co.uk>
2024-12-31 11:25:39 +0000

    Create Remove MyHeritage Junk Matches.js

---
+++ Remove MyHeritage Junk Matches.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Remove MyHeritage Junk Matches
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Hides low confidence matches from MyHeritage's DNA match list to make browsing more efficient
+// @author       You
+// @match        https://www.myheritage.com/dna/matches/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=myheritage.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function doIt(){
+        document.querySelectorAll("div.dna_match_card").forEach(function(e){
+            if(e.innerText.indexOf("confidence") !== -1){
+                e.remove();
+            }
+        })
+        window.setTimeout(doIt, 1000)
+    }
+    window.setTimeout(doIt, 1000)
+})();
